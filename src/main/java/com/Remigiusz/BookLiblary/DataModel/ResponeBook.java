c29e79d8d595ef533affe9bd7c13791eebc3eff1
RemigiuszWyciszkiewicz <remigiuszwyciszkiewicz000@gmail.com>
2019-05-11 18:52:09 +0200

    Responebook

--- src/main/java/com/Remigiusz/BookLiblary/DataModel/ResponeBook.java
+++ src/main/java/com/Remigiusz/BookLiblary/DataModel/ResponeBook.java
@@ -1,4 +1,22 @@
 package com.Remigiusz.BookLiblary.DataModel;
 
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class ResponeBook {
+
+    private String isbn;
+    private String title;
+    private String subtitle;
+    private long publishedDate;
+    private String description;
+    private int pageCount;
+    private String thumbnailUrl;
+    private Set<String> authors;
+    private Set<String> categories;
+
+
+
 }
