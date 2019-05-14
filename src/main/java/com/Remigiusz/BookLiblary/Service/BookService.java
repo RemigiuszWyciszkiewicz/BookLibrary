34aadde51e7a3ab9eae18640a0675605ab709c11
RemigiuszWyciszkiewicz <remigiuszwyciszkiewicz000@gmail.com>
2019-05-14 14:36:20 +0200

    tests

--- src/main/java/com/Remigiusz/BookLiblary/Service/BookService.java
+++ src/main/java/com/Remigiusz/BookLiblary/Service/BookService.java
@@ -1,14 +1,14 @@
 package com.Remigiusz.BookLiblary.Service;
 
 
-import com.Remigiusz.BookLiblary.Response.AuthorRatingResponse;
-import com.Remigiusz.BookLiblary.Response.BookResponse;
-import org.springframework.stereotype.Service;
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+        import com.Remigiusz.BookLiblary.Response.AuthorRatingResponse;
+        import com.Remigiusz.BookLiblary.Response.BookResponse;
+        import org.springframework.stereotype.Service;
+        import javax.annotation.PostConstruct;
+        import java.io.IOException;
+        import java.util.*;
+        import java.util.stream.Collectors;
+        import java.util.stream.IntStream;
 
 
 @Service
@@ -24,12 +24,12 @@ public class BookService {
 
     }
 
-   
+
 
     public Optional<BookResponse> getBookByISBN(String isbn) {
 
-       Optional<BookResponse> optionalResponeBook =
-               listOfBooksFromGoogleApi.stream().filter(responeBook -> responeBook.getIsbn().equals(isbn)).findAny();
+        Optional<BookResponse> optionalResponeBook =
+                listOfBooksFromGoogleApi.stream().filter(responeBook -> responeBook.getIsbn().equals(isbn)).findAny();
 
         return optionalResponeBook;
     }
@@ -68,20 +68,20 @@ public class BookService {
                 .forEach(index_1 -> { IntStream.range(0, bookWithoutNulls.size()).forEach( index_2 -> {
 
                     if(bookWithoutNulls.get(index_2).getAuthors().contains(authors.get(index_1)))
-                    authorRatingResponsesList.add(new AuthorRatingResponse(authors.get(index_1),bookWithoutNulls.get(index_2).getAverageRating()));
+                        authorRatingResponsesList.add(new AuthorRatingResponse(authors.get(index_1),bookWithoutNulls.get(index_2).getAverageRating()));
 
                 } );
-        });
+                });
 
         // 5 - Grouping by author
         Map<String, Double> averages = authorRatingResponsesList.stream().collect(Collectors.groupingBy(e -> e.getAuthor(),
-               Collectors.averagingDouble(value -> value.getRating())));
+                Collectors.averagingDouble(value -> value.getRating())));
 
 
         // 6 - Sorting descending and maping Map into BookResponse list
         List<AuthorRatingResponse> authorRatingResponses = averages.entrySet().stream()
                 .sorted(Map.Entry.<String, Double>comparingByValue().reversed()).map(stringDoubleEntry -> {
-                 return new AuthorRatingResponse(stringDoubleEntry.getKey(),stringDoubleEntry.getValue());
+                    return new AuthorRatingResponse(stringDoubleEntry.getKey(),stringDoubleEntry.getValue());
                 } ).collect(Collectors.toList());
 
 
